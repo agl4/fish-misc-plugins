8e17692bdfafddc6d2e39d486819eeded80d701c
Attila GOLONCSER <agl@kxxp.net>
2022-08-02 13:07:35 +0200

    Update bindings

--- conf.d/pooldir.fish
+++ conf.d/pooldir.fish
@@ -1,5 +1,5 @@
 set -q POOLDIR_ROOT; or set -U POOLDIR_ROOT "$HOME/share/pool2"
-bind \cp __pooldir
+bind \co __pooldir
 if ! test "$fish_key_bindings" = fish_default_key_bindings
-    bind -M insert \cp __pooldir
+    bind -M insert \co __pooldir
 end
