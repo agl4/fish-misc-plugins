a544bacf2eccf6d2e057c5620b04e75e3a03b3dd
Attila GOLONCSER <agl@kxxp.net>
2022-08-02 08:41:15 +0200

    feat: Add cdp command

---
+++ conf.d/cdp.fish
@@ -0,0 +1,5 @@
+set -q CDP_PROJECT_DIR; or set -U CDP_PROJECT_DIR "$HOME/src"
+bind \co __cdp_insert_project
+if ! test "$fish_key_bindings" = fish_default_key_bindings
+    bind -M insert \co __cdp_insert_project
+end
